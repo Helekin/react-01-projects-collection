bf76e5651cd760310be8658271a784d4509c060e
Helekin <clsjos2294@gmail.com>
2026-05-05 17:27:19 -0500

    UPDATE: Form

--- react-04-calorie-counter/src/components/Form.tsx
+++ react-04-calorie-counter/src/components/Form.tsx
@@ -11,9 +11,11 @@ export default function Form() {
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>,
   ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
     setActivity({
       ...activity,
-      [e.target.id]: e.target.value,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
   };
 
